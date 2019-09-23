198e5cb049bc28c7b90e01a585b47b78fd43b57a
YaoHua Tan <yaohuatan@Shinkai.local>
2019-09-23 16:08:53 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TYHome' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TYHome
+  pod 'CTMediator'
+end
---
+++ TYHome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TYHome
+//
+//  Created by YaoHua Tan on 2019/9/23.
+//  Copyright © 2019 YaoHua Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
